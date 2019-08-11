073390d9d5e488babf9461479332be99715e20b7
Willy <dorgonway@gmail.com>
2019-08-11 23:25:47 +0800

    Add files via upload

--- django_tutorial/musics/views.py
+++ django_tutorial/musics/views.py
@@ -8,9 +8,32 @@ import musics.countpy
 # Create your views here.
 def hello_view(request):
     a = musics.countpy.shownum(3)
-    
-    return render(request, 'index.html', {
-            'data': "Hello Django ",
-            'number_pkg':str(a),
-    })
+    try:
+        if request.method == 'POST':
+            if  request.POST.get('excel_path') and request.POST.get('edf_path') :
+                excel_path= request.POST.get('excel_path')
+                edf_path= request.POST.get('edf_path')
+        
+                return render(request, 'index.html', {
+                'data': "Hello Django WORKing!!!! ",
+                'number_pkg':str(a),
+                'EXCEL_PATH':excel_path,
+                'EDF_PATH':edf_path,
+                })
+            else:
+                 return render(request, 'index.html', {
+                    'data': "Hello Django NOT WORK request = True ",
+                    'number_pkg':str(a),
+                    })
+        else:
+            return render(request, 'index.html', {
+                    'data': "Hello Django NOT WORK ",
+                    'number_pkg':str(a),
+            })
+    except:
+        return render(request, 'index.html', {
+        'data': "Hello Django NOT WORK ",
+        'number_pkg':str(a),
+            })
+
     
